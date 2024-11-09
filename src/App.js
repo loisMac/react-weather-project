facf9fd8487521034555c5a027cd8a4d10f4846c
loisMac <155264617+loisMac@users.noreply.github.com>
2024-11-09 20:11:31 +0000

    Added norefferer to link

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
       <footer>
         This project was coded by Lois Macleod and is open-sourced on {" "}
       <a href="https://github.com/loisMac/react-weather-project"
-      target="_blank">Github</a>
+      target="_blank" rel="noreferrer">Github</a>
       </footer>
     </div>
   );
