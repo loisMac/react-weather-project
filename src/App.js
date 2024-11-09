ab8633b68dfb3f0eb7a65cfa026ec879e5620138
loisMac <155264617+loisMac@users.noreply.github.com>
2024-11-09 20:22:29 +0000

    Added bootstrap via terminal

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <div className="container">
+      <h1>Weather App</h1>
       <footer>
         This project was coded by Lois Macleod and is open-sourced on {" "}
       <a href="https://github.com/loisMac/react-weather-project"
       target="_blank" rel="noreferrer">Github</a>
       </footer>
+      </div>
     </div>
   );
 }
