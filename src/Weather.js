b3674ab7353942ce68390c887737f5c81aa9f759
loisMac <155264617+loisMac@users.noreply.github.com>
2024-11-10 12:58:20 +0000

    worked on css for weather content

--- src/Weather.js
+++ src/Weather.js
@@ -9,10 +9,11 @@ export default function Weather() {
                     <div className="col-9">
                 <input type="search" 
                 placeholder="Enter a city.." 
-                className="form-control" />
+                className="form-control"
+                autoFocus="on" />
                 </div>
                 <div className="col-3">
-                <input type="submit" value="Search" className="btn btn-primary" />
+                <input type="submit" value="Search" className="btn btn-primary w-100" />
                 </div>
                 </div>
             </form>
@@ -25,11 +26,15 @@ export default function Weather() {
                     Mostly Cloudy
                 </li>
             </ul>
-            <div className="row">
-                <div className="col-6">
-                    <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAdVJREFUaN7tmc1thDAQRimBElwCJVBCSvAxR5fgEiiBEiiBErhyIx24A2cc2WhiAf4ZA1rJkZ4UZZPN9/AwHrON1rr5ZJoqUAWqQBWoAlWgxJf++WaAAGZAAdpD2dfM7zDS/yopAGE6YDoIHMLIdK8KQIAWGIAtQ8Bh/r59bQWQjCBILCkSJIF1XVuAA9Jivm9ROd0ukS0AQTtgA7SH+Vn31EoEBSAMA2YUUAHiJDyWcCtBuidIArZEroJewVEpjQSJjiIgMsMbpHdjf53sCcEWSxEYCQKOyZQhkshZBZYkYEtHeLVPQSGJnHIS0QI2/FIo+L+VILTXOUVA3BD+D3Q/pAqoFIEebUxFQQLJN/Ojo0TEqDG/JgBv1hdgeVNAP4CKPSvkCKiCQc1KSMRs2+x902hO/Z4cYFhgWOQHY8zo9hOKgCCGH71BEXcqHjEBKDft5gowypVH4YeLgKE9ZSO10cxz7z7TFJqxOEUgZxyYbPi+0M4uSRuZPYCnCPBA6TwrYCWWyFbJImo/FTMpM6pAG5CYvDO0LDii7x2JNAtdSGxuQyp41Q87UqkHW8NJzYsbw+8d6Y5Hi+7qbw8IyOIPd9HRVD8qUD8fqAJVoApUgSrwqfwCJ6xaZshM+xMAAAAASUVORK5CYII="
-                    alt="mostly cloudy"/>
-                    6°C
+            <div className="row mt-3">
+                <div className="col-6 img-temp">
+                
+                    <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+                    alt="Sunny" />
+                
+                    <span className="temperature">14</span>
+                    <span className="unit">°C</span>
+                    
                 </div>
                 <div className="col-6">
                     <ul>
